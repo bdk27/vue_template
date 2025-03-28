3726747a2b097998c63f6f5be4e127dd35c3fbdf
YCheng <briansam195@gmail.com>
2025-03-28 14:24:48 +0800

    新增 Test 元件，更新 App.vue 以顯示 Test 元件，並在 Vite 配置中新增路徑別名

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import Test from "@/components/Test.vue";
+</script>
 
 <template>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <Test />
 </template>
 
 <style></style>
